4ad0ba906b514a6a350877bb309cb07ff600de59
Kylie <kylieavie@gmail.com>
2022-11-19 17:47:22 +0800

    added search term in input

--- components/Header.tsx
+++ components/Header.tsx
@@ -38,10 +38,11 @@ function Header() {
 
         <form className="flex border border-gray-200 rounded-full shadow-lg max-w-3xl items-center px-6 py-3 ml-10 mr-5 flex-grow">
           <input
+            defaultValue={router.query.term}
             type="text"
             ref={searchInputRef}
             className="flex-grow w-full focus:outline-none"
-          />  
+          />
           <XMarkIcon
             title="Clear"
             onClick={() => (searchInputRef!.current!.value = "")}
